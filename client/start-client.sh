214147ee7b9962f840302b816e51058362b67974
Iyappan Subramanian <isubramanian@amperecomputing.com>
2024-05-10 20:46:41 +0000

    Create a brand new branch using ollama and openwebui

--- client/start-client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-npm run preview -- --host 0.0.0.0 --port 3000
--- server/download-model.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-model_file=`basename $MODEL_URL`
-
-if [ ! -f $model_file ]; then
-    echo "Downloading model: $model_file"
-    echo $MODEL_URL | tr -d '"' | xargs wget --no-check-certificate -
-fi
