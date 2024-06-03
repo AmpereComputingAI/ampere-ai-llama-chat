d89d503e480d3c7242b6d7bec959a218de3d2e31
Iyappan Subramanian <isubramanian@amperecomputing.com>
2024-06-03 22:05:54 +0000

    Added patch for torch.compile

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/open-webui/open-webui.git
 pushd open-webui
 git checkout -b v0.1.124 v0.1.124
+git apply --3way ../patch/rag-main-py.patch
 popd
 
 docker compose build open-webui
