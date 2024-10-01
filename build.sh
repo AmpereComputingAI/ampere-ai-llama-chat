7a6d88870175a0479d3464c75ba9ddcad3859301
Iyappan Subramanian <isubramanian@amperecomputing.com>
2024-10-01 22:21:11 +0000

    Update softwares
    - Bump open-webui==v0.3.29
    - Add support Milvus database
    - Add support for newer models

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 git clone https://github.com/open-webui/open-webui.git
 pushd open-webui
-git checkout -b v0.3.11 v0.3.11
+git checkout -b v0.3.29 v0.3.29
 git apply --3way ../patch/rag-main-py.patch
+git apply --3way ../patch/utils-utils-py.patch
 popd
 
 docker compose build open-webui
